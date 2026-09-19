ee80c389c683d87e733bd054a6c98b40d2578f37
raeandres <raeandres@users.noreply.github.com>
2026-09-19 03:38:16 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict d8ZZ5mDKTH0fBF3Nr9d8IhxDocOiblza0A62KDZigpbcHFD0WcOOA644oJJtfJl
+-- \restrict DF1F1wvYYwQhcWHbHzMGzQGcphWMmGNTvJKn6dHGIkFo4idnBF60yFBuIbNztFF
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -4454,6 +4454,6 @@ SELECT pg_catalog.setval('"auth"."refresh_tokens_id_seq"', 1, false);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict d8ZZ5mDKTH0fBF3Nr9d8IhxDocOiblza0A62KDZigpbcHFD0WcOOA644oJJtfJl
+-- \unrestrict DF1F1wvYYwQhcWHbHzMGzQGcphWMmGNTvJKn6dHGIkFo4idnBF60yFBuIbNztFF
 
 RESET ALL;
